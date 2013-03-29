ee748ac8a13089fcb08794795700ee9246505a97
Xursian <dranger43@academic.rrc.ca>
2013-03-29 12:45:49 -0500

    created rails application for estore

---
+++ e-store/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EStore::Application.config.secret_token = 'e6740fa45bee1c7e6c082d10ccd6e9c98c2e91b7843fb556504042cb30ff223c60e19cb16e5bd703b6ee70fc56de0cf10b735ee06cd6b3d6be517b9b24086232'
